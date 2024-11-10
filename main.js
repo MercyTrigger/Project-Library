1081e5b7bb437266771a6e6ad3f0b7aeb5a7143d
MercyTrigger <{151790852}+{MercyTrigger}@users.noreply.github.com>
2024-11-11 00:09:58 +0300

    Add form for book data

---
+++ main.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(book) {
+    this.book = book;
+}
+
+
+function addBookToLibraary(book) {
+    newBook.prototype.book() {
+        Book.call(book)
+    }
+}
+
+
