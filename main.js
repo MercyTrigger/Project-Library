339b8ff1e49e34c2cf532a1f8f62b512c785ade3
MercyTrigger <{151790852}+{MercyTrigger}@users.noreply.github.com>
2024-11-14 17:00:57 +0300

    Add a assets folder and modal window

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const myLibrary = [];
+// modal box section
+const dialog = document.querySelector("dialog")
+
+const addBook = document.querySelector(".add-book");
+
+const exitModal = document.querySelector(".exit-modal");
+
+addBook.addEventListener("click", () => {
+    document.querySelector(".parent-dialog").style.display = "flex"
+    dialog.showModal()
+})
+
+exitModal.addEventListener("click", () => {
+    dialog.close()
+    document.querySelector(".parent-dialog").style.display = "none"
+})
+
+
+
+/** const myLibrary = [];
 
 function Book(book) {
     this.book = book;
@@ -10,5 +29,8 @@ function addBookToLibraary(book) {
         Book.call(book)
     }
 }
+
+*/
+
 
 
